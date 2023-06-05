7ab84dedf4074d7bec72b2433bf43c900a6fa952
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-06-05 22:04:17 +0200

    Second Try

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,4 +1,47 @@
+#include <stdio.h>
 #include "lists.h"
+
+void print_seen(size_t **seen, int num)
+{
+	int i;
+	for (i = 0; i < num; i++)
+		printf("\ncount = %d ,seen[%d] = %lu", num, i, *seen[i]);
+}
+
+void init_seen(size_t **seen, listint_t **list, int *count)
+{
+	*seen = malloc(sizeof(size_t) * 1);
+	if (*seen == NULL)
+		exit(-1);
+	(*seen)[0] = (size_t) *list;
+	*count = *count + 1;
+}
+
+int has_been_seen(size_t **seen, listint_t **list, int count)
+{
+	int i;
+
+	for (i = 0; i < count - 1; i++)
+		if ((*seen)[i] == ((size_t) *list))
+			return (1);
+	return (0);
+}
+
+void add_to_seen(size_t **seen, listint_t **list, int *count)
+{
+	size_t *new_seen;
+	int i;
+
+	*count = *count + 1;
+	new_seen = malloc(sizeof(size_t) * (*count));
+	if (new_seen == NULL)
+		exit(-1);
+	for (i = 0; i < *count - 1; i++)
+		new_seen[i] = (*seen)[i];
+	new_seen[*count - 1] = (size_t) (*list);
+	free(*seen);
+	*seen = new_seen;
+}
 
 /**
  * check_cycle - function that checks if a singly linked list has a cycle in it
@@ -12,33 +55,25 @@
  */
 int check_cycle(listint_t *list)
 {
-	size_t *seen_addresses = NULL, *temp_addresses = NULL;
+	size_t *seen = NULL;
 	int count = 0;
-	int i;
-
 	while (list != NULL)
 	{
-		if (count > 0)
+		if (count == 0)
 		{
-			temp_addresses = malloc(sizeof(size_t) * (count + 1));
-			if (temp_addresses == NULL)
-				return (0);
-			for (i = 0; i < count; i++)
-				temp_addresses[i] = seen_addresses[i];
-			free(seen_addresses);
-			seen_addresses = NULL;
-			for (i = 0; i < (count - 1); i++)
-				if (temp_addresses[i] == (size_t) list)
-					return (0);
+			init_seen(&seen, &list, &count);
 		}
-		count++;
-		seen_addresses = malloc(sizeof(size_t) + (count + 1));
-		if (seen_addresses == NULL)
-			return (0);
-		for (i = 0; i < (count - 1); i++)
-			seen_addresses[i] = temp_addresses[i];
-		seen_addresses[count - 1] = (size_t) list;
+		else
+		{
+			if (has_been_seen(&seen , &list, count))
+			{
+				free(seen);
+				return (0);
+			}
+			add_to_seen(&seen, &list, &count);
+		}
 		list = list->next;
 	}
+	free(seen);
 	return (1);
 }
