dd08570677e97add869fd5549c76782951eb14b0
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-06-05 22:32:41 +0200

    Hare and Tortoise Algo

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,91 +1,4 @@
-#include <stdio.h>
 #include "lists.h"
-
-/**
- * print_seen - prints seen array
- *
- * Description: prints seen array
- *
- * @seen: seen array
- * @num: number of things to print
- *
- * Return: void
- */
-void print_seen(size_t **seen, int num)
-{
-	int i;
-
-	for (i = 0; i < num; i++)
-		printf("\ncount = %d ,seen[%d] = %lu", num, i, *seen[i]);
-}
-
-/**
- * init_seen - prints seen array
- *
- * Description: prints seen array
- *
- * @seen: seen array
- * @list: list pointer
- * @count: number of things to print
- *
- * Return: void
- */
-void init_seen(size_t **seen, listint_t **list, int *count)
-{
-	*seen = malloc(sizeof(size_t) * 1);
-	if (*seen == NULL)
-		exit(-1);
-	(*seen)[0] = (size_t) *list;
-	*count = *count + 1;
-}
-
-/**
- * has_been_seen - prints seen array
- *
- * Description: prints seen array
- *
- * @seen: seen array
- * @list: list pinter
- * @count: number of things to print
- *
- * Return: 1 for True, 0 for false
- */
-int has_been_seen(size_t **seen, listint_t **list, int count)
-{
-	int i;
-
-	for (i = 0; i < count - 1; i++)
-		if ((*seen)[i] == ((size_t) *list))
-			return (1);
-	return (0);
-}
-
-/**
- * add_to_seen - prints seen array
- *
- * Description: prints seen array
- *
- * @seen: seen array
- * @list: list pointer
- * @num: number of things to print
- *
- * Return: void
- */
-void add_to_seen(size_t **seen, listint_t **list, int *count)
-{
-	size_t *new_seen;
-	int i;
-
-	*count = *count + 1;
-	new_seen = malloc(sizeof(size_t) * (*count));
-	if (new_seen == NULL)
-		exit(-1);
-	for (i = 0; i < *count - 1; i++)
-		new_seen[i] = (*seen)[i];
-	new_seen[*count - 1] = (size_t) (*list);
-	free(*seen);
-	*seen = new_seen;
-}
 
 /**
  * check_cycle - function that checks if a singly linked list has a cycle in it
@@ -99,27 +12,20 @@ void add_to_seen(size_t **seen, listint_t **list, int *count)
  */
 int check_cycle(listint_t *list)
 {
-	size_t *seen = NULL;
-	int count = 0;
+	listint_t *hare, *tortoise;
 
-	while (list != NULL)
+	if (list == NULL)
+		return (0);
+
+	tortoise = list;
+	hare = list->next;
+
+	while (tortoise != hare)
 	{
-		if (count == 0)
-		{
-			init_seen(&seen, &list, &count);
-		}
-		else
-		{
-			if (has_been_seen(&seen, &list, count))
-			{
-				free(seen);
-				return (0);
-			}
-			add_to_seen(&seen, &list, &count);
-		}
-		list = list->next;
+		if (hare->next == NULL || hare->next->next == NULL)
+			return (0);
+		tortoise = tortoise->next;
+		hare = hare->next->next;
 	}
-	if (count > 0)
-		free(seen);
 	return (1);
 }
