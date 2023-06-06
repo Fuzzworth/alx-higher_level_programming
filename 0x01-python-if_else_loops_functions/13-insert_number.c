13d9072226daadf5e0fc269175964978d8042cd0
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-06-06 17:57:20 +0200

    Insert in sorted linked list

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * insert_node - short description
+ *
+ * Description: long description
+ *
+ * @head: argument_1 description
+ * @number: argument_2 description
+ *
+ * Return: return description
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	return (0);
+}
