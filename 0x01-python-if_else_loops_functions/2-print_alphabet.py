fbbf925210ec38bd9cbf5d8b78fde4d303ac9d1f
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-06-06 14:57:15 +0200

    Fix:3

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for character in range(97,123):
-    print("{}".format(chr(character), end="")
+for char_number in range(97, 123):
+    print("{character}".format(character=chr(char_number)), end="")
