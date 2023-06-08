b5d767eb7433a3d2cc08482a60eeb6820264a540
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-06-08 18:54:51 +0200

    Write a program that prints all the names defined by the compiled module hidden_4.pyc (please download it locally). FIX : 2

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 import importlib.util
 
-module_name = 'hidden'  # Replace with your .pyc module name (without the extension)
-module_path = './hidden_4.pyc'  # Replace with the actual path to your .pyc file
+module_name = 'hidden_4'
+module_path = './hidden_4.pyc'
 
 spec = importlib.util.spec_from_file_location(module_name, module_path)
 module = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(module)
-# Use dir() to get the attributes and methods of the module
-module_attributes = dir(module)
 
-# Filter and print only the lines that don't start with "__"
-filtered_attributes = [attr for attr in module_attributes if not attr.startswith('__')]
-for attribute in filtered_attributes:
-    print(attribute)
+names = [name for name in dir(module) if not name.startswith('__')]
+names.sort()
+
+for name in names:
+    print(name)
+
