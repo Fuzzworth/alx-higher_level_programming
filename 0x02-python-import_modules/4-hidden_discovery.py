2c24752c5654cb29f5d08bc65465956acd6ac038
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-06-08 20:40:02 +0200

    Fix: 10

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,16 +1,3 @@
 #!/usr/bin/python3
-import importlib.util
-
-module_name = 'hidden_4'
-module_path = './hidden_4.pyc'
-
-spec = importlib.util.spec_from_file_location(module_name, module_path)
-module = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(module)
-
-names = [name for name in dir(module) if not name.startswith('__')]
-names.sort()
-
-for name in names:
-    print(name)
-
+import hidden_4
+print(dir())
