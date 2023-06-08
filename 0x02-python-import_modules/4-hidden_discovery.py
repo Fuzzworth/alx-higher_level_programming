57173095bedb18a2c5ea43a7a99adb4ee2c25909
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-06-08 18:16:29 +0200

    Write a program that prints all the names defined by the compiled module hidden_4.pyc (please download it locally). FIX : 1

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,5 @@ import hidden_4
 modules = dir()
 if __name__ == "__main__":
     for module in modules:
-        if (!module.statswith("__")):
+        if (not module.statswith("__")):
             print(module)
