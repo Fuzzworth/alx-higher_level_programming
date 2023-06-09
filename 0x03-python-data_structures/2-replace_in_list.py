642c1515004b2b88b3cb54ccd45831cf1d510b54
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-06-09 23:40:58 +0200

    Write a function that replaces an element of a list at a specific position (like in C).

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -5,6 +5,5 @@ def replace_in_list(my_list, idx, element):
     elif int(idx) > (len(my_list) - 1):
         return None
     else:
-        new_list = my_list.copy()
-        new_list[int(idx)] = element
+        my_list[int(idx)] = element
         return new_list
