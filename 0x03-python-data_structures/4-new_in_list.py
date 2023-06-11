e6e364ec9e462ac35d68da74fb08a85dd4932f5e
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-06-11 21:47:17 +0200

    checker fix: 2;task 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if int(idx) < 0 orint(idx) > (len(my_list) - 1):
+    if int(idx) < 0 or int(idx) > (len(my_list) - 1):
         return my_list.copy()
     else:
         new_list = my_list.copy()
