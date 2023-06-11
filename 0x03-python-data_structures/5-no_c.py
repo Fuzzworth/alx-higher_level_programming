55cce962a028e05511c60b527fbe440a9f64c2a9
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-06-11 21:49:07 +0200

    checker fix: 1;task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 def no_c(my_string):
     new_str = ""
-    for i in my_string:
-        if ord(i) != ord('c') and ord(i) != ord('C'):
-            new_str += str(i)
+    if my_string:
+        for i in my_string:
+            if ord(i) != ord('c') and ord(i) != ord('C'):
+                new_str += str(i)
     return new_str
