29c8ba8eeadd0b7afd6773ad8d934b85da255c6a
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-06-11 23:46:23 +0200

    Linked list palindrome FIX: 4

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -29,7 +29,7 @@ int is_palindrome(listint_t **head)
 		mid = (count / 2) + 1;
 	for (i = 0; i < mid; i++, current = current->next)
 		list_array[i] = current->n;
-	for (i = i; i >= 0; i--, current = current->next)
+	for (i = i - 1; i >= 0; i--, current = current->next)
 		if (list_array[i] != current->n)
 			return (0);
 	return (1);
