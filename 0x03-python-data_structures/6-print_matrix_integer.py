667827623a2a6beea9b3065c5ffabb98e44751eb
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-06-11 21:54:12 +0200

    checker fix: 1;task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for array in range(len(matrix)):
-        for element in range(len(matrix[array])):
-            if element != (len(matrix[array]) - 1):
-                print("{:d}".format(matrix[array][element]), end=" ")
-            else:
-                print("{:d}".format(matrix[array][element]), end="\n")
+    if matrix:
+        for array in range(len(matrix)):
+            if matrix[array]:
+                for element in range(len(matrix[array])):
+                    if element != (len(matrix[array]) - 1):
+                        print("{:d}".format(matrix[array][element]), end=" ")
+                    else:
+                        print("{:d}".format(matrix[array][element]), end="\n")
