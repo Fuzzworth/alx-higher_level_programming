ddb6fd4389202acde1084f851fd6e3a8d999b27e
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-06-11 14:37:52 +0200

    checker fix: 3

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if int(idx) < 0 or int(idx) > (len(my_list)):
-        return mylist
+    if int(idx) < 0 or int(idx) > (len(my_list) - 1):
+        return my_list
     else:
         my_list[int(idx)] = element
         return my_list
