b292d6cf04e789b9d6ac981810fcb240835a2c53
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-06-11 14:51:17 +0200

    checker fix: 6; task 3:

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,4 +3,4 @@ def print_reversed_list_integer(my_list=[]):
     if len(my_list) > 0:
         my_list.reverse()
         for element in my_list:
-            print("{:d}".format(element))
+            print("{}".format(element))
