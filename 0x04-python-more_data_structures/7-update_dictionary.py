b818593fb6e7314e00f170b425956bdf7a163bfb
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-06-14 22:19:33 +0200

    Write a function that replaces or adds key/value in a dictionary.

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    if key in a_dictionary:
-        a_dictionary.update(key=value)
+    new_dictionary = a.a_dictionary.copy()
+    if key in new_dictionary:
+        new_dictionary.update(key=value)
     else:
-        a_dictionary[key] = value
+        new_dictionary[key] = value
+    return new_dictionary
