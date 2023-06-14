8d16cb0428d14d8b5669217c8298d94aa0934dc1
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-06-15 00:55:56 +0200

    fix: 1

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -2,9 +2,8 @@
 def complex_delete(a_dictionary, value):
     new_dict = {}
     for key, val in a_dictionary.items():
-        if value != val:
+        if value == val:
             new_dict[key] = value
-    a_dictionary.clear()
     for key, val in new_dict.items():
-        a_dictionary[key] = val
+        del a_dictionary[key]
     return new_dict
