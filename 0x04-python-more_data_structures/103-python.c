4a096099e7f1ae76eb9c6908496fc31deb4320ca
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-06-15 03:19:25 +0200

    Create two C functions that print some basic info about Python lists and Python bytes objects. Fix: 2

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -3,39 +3,6 @@
 #include <object.h>
 #include <bytesobject.h>
 
-void print_python_list(PyObject *p);
-void print_python_bytes(PyObject *p);
-
-/**
- * print_python_list - prints some basic info about Python lists
- *
- * Description: function that prints some basic info about Python lists
- *
- * @p: PyObject pointer
- *
- * Return: void
- */
-void print_python_list(PyObject *p)
-{
-	long int size_of_list = PyList_GET_SIZE(p), list_index;
-	PyObject *temp;
-	PyListObject *list_object = (PyListObject *) p;
-
-	printf("[*] Python list info\n");
-	printf("[*] Size of the Python List = %ld\n", size_of_list);
-	printf("[*] Allocated = %ld\n", list_object->allocated);
-
-	for (list_index = 0; list_index < size_of_list; list_index++)
-	{
-		temp = PyList_GET_ITEM(p, list_index);
-		if (PyBytes_CheckExact(temp))
-			print_python_bytes(temp);
-		else
-			printf("Element %ld: %s\n", list_index,
-					temp->ob_type->tp_name);
-	}
-}
-
 /**
  * print_python_bytes - prints python bytes
  *
@@ -66,4 +33,34 @@ void print_python_bytes(PyObject *p)
 	}
 	else
 		printf("  [ERROR] Invalid Bytes Object\n");
+}
+
+/**
+ * print_python_list - prints some basic info about Python lists
+ *
+ * Description: function that prints some basic info about Python lists
+ *
+ * @p: PyObject pointer
+ *
+ * Return: void
+ */
+void print_python_list(PyObject *p)
+{
+	long int size_of_list = PyList_GET_SIZE(p), list_index;
+	PyObject *temp;
+	PyListObject *list_object = (PyListObject *) p;
+
+	printf("[*] Python list info\n");
+	printf("[*] Size of the Python List = %ld\n", size_of_list);
+	printf("[*] Allocated = %ld\n", list_object->allocated);
+
+	for (list_index = 0; list_index < size_of_list; list_index++)
+	{
+		temp = PyList_GET_ITEM(p, list_index);
+		if (PyBytes_CheckExact(temp))
+			print_python_bytes(temp);
+		else
+			printf("Element %ld: %s\n", list_index,
+					temp->ob_type->tp_name);
+	}
 }
