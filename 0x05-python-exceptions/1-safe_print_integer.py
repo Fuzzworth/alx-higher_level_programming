8186d582d1c2181a92766e7655faefce2979c680
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-06-26 18:26:21 +0200

    Write a function that prints an integer with "{:d}".format() FIX: 3

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
+        if value is None:
+            raise ValueError
         print("{:d}".format(value))
         return True
     except ValueError:
