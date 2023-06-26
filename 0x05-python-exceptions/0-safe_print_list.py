c7565071bf38ed417290e7cc08f9f6e131bde6b9
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-06-26 18:12:42 +0200

    Write a function that prints x elements of a list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,7 +2,7 @@
 def safe_print_list(my_list=[], x=0):
     count = 0
     try:
-        while x >= 0:
+        while x > 0:
             print("{}".format(my_list[count]), end="")
             count += 1
             x -= 1
