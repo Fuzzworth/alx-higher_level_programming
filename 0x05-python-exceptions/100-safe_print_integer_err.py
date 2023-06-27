433fa1ffc0e033d1c9a672f253847cf35572746a
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-06-27 03:58:56 +0200

    Into the void 100

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from sys import exc_info, stderr
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+    except (ValueError, TypeError):
+        print("Exception: {}".format(exc_info()[1], file=stderr))
+    return isinstance(value, int)
