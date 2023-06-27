c97500d5496fb987e3b00b447bf5df1bd8822b7f
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-06-27 14:13:25 +0200

    Write a class Square that defines a square by: (based on 4-square.py)

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -75,7 +75,7 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
 
-    def area(self):
+    def my_print(self):
         """
         Note:
             Do not include the `self` parameter in the ``Args`` section.
