991415ab1743c8ab64d73bba3bd332fb2cf39516
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-06-27 12:55:56 +0200

    Write a class Square that defines a square by: (based on 0-square.py)

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,11 +3,13 @@
 This is a module that Write a class Square that defines a square by: (based on 0-square.py)
 """
 
+
 class Square:
     """Function that defines a square"""
     def __init__(self, size):
         self.__size = size
         """
         Private instance attribute: size
-        Instantiation with size (no type/value verification)
+        Instantiation with size
+        (no type/value verification)
         """
