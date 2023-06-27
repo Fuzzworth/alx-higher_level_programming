20171c3a38ac645637a431d80b321d568e87398f
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-06-27 14:30:17 +0200

    Write a class Square that defines a square by: (based on 5-square.py)

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -33,10 +33,10 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
         if not isinstance(position, tuple) or len(position) != 2
-            or not isinstance(position[0], int)
-            or not isinstance(position[1], int)
-            or position[0] < 0 or position[1] < 0:
-                raise TypeError("position must be a tuple of 2
+        or not isinstance(position[0], int)
+        or not isinstance(position[1], int)
+        or position[0] < 0 or position[1] < 0:
+            raise TypeError("position must be a tuple of 2
                                 positive integers")
         self.__position = position
         """
