c12b2a5cfc32d9388b82597d94ca81710df1dbfb
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-06-30 01:55:15 +0200

    Write a class Node that defines a node of a singly linked list by:

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,61 +1,40 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
 """
-This module demonstrates documentation as specified by the `Google Python
-Style Guide`_. Docstrings may extend over multiple lines. Sections are created
-with a section header and a colon followed by a block of indented text.
-
-Example:
-    Examples can be given using either the ``Example`` or ``Examples``
-    sections. Sections support any reStructuredText formatting, including
-    literal blocks::
-
-        $ python example_google.py
-
-Section breaks are created by resuming unindented text. Section breaks
-are also implicitly created anytime a new section starts.
-
-Attributes:
-    module_level_variable1 (int): Module level variables may be documented in
-        either the ``Attributes`` section of the module docstring, or in an
-        inline docstring immediately following the variable.
-
-        Either form is acceptable, but the two should not be mixed. Choose
-        one convention to document module level variables and be consistent
-        with it.
-
-Todo:
-    * For module TODOs
-    * You have to also use ``sphinx.ext.todo`` extension
-
-.. _Google Python Style Guide:
-   http://google.github.io/styleguide/pyguide.html
-
+Write a class Node that defines a node of a singly linked list by: 
 """
 
 
-class Node(object):
-    """Exceptions are documented in the same way as classes.
-
-    The __init__ method may be documented in either the class level
-    docstring, or as a docstring on the __init__ method itself.
-
-    Either form is acceptable, but the two should not be mixed. Choose one
-    convention to document the __init__ method and be consistent with it.
+class Node:
+    """
+    Node Class
 
     Note:
         Do not include the `self` parameter in the ``Args`` section.
 
-    Args:
-        msg (str): Human readable string describing the exception.
-        code (:obj:`int`, optional): Error code.
-
     Attributes:
-        msg (str): Human readable string describing the exception.
-        code (int): Exception error code.
+        data (int): Human readable string describing the exception.
+        next_node (Node): Exception error code.
     """
 
     def __init__(self, data, next_node=None):
+        """
+        The __init__ method may be documented in either the class level
+        docstring, or as a docstring on the __init__ method itself.
+
+        Either form is acceptable, but the two should not be mixed. Choose one
+        convention to document the __init__ method and be consistent with it.
+
+        Note:
+            Do not include the `self` parameter in the ``Args`` section.
+
+        Args:
+            data (int): Description of `param1`.
+            next_node (:obj:`Node`, optional): Description of `param2`. Multiple
+        
+        Raises:
+            TypeError: that are relevant to the interface.
+        """
+
         if not isinstance(data, int):
             raise TypeError("data must be an integer")
         self.__data = data
@@ -65,53 +44,70 @@ class Node(object):
 
     @property
     def data(self):
+        """int: data"""
         return self.__data
 
     @data.setter
     def data(self, value):
+        """
+        Setter for data
+
+        Note:
+            Do not include the `self` parameter in the ``Args`` section.
+
+        Args:
+            value (int): Description of `param1`.
+        
+        Raises:
+            TypeError: that are relevant to the interface.
+        """
+
         if not isinstance(value, int):
             raise TypeError("data must be an integer")
         self.__data = value
 
     @property
     def next_node(self):
+        """:obj:`Node`: Node"""
         return self.__next_node
 
     @next_node.setter
     def next_node(self, value):
+        """
+        Setter for next_node
+
+        Note:
+            Do not include the `self` parameter in the ``Args`` section.
+
+        Args:
+            value (:obj:`Node`:): Description of `param1`.
+        
+        Raises:
+            TypeError: that are relevant to the interface.
+        """
+
         if value is not None and not isinstance(value, Node):
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
 
     def __str__(self):
+        """str: data string"""
         return str(self.__data)
 
 
 class SinglyLinkedList(object):
-    """Exceptions are documented in the same way as classes.
-
-    The __init__ method may be documented in either the class level
-    docstring, or as a docstring on the __init__ method itself.
-
-    Either form is acceptable, but the two should not be mixed. Choose one
-    convention to document the __init__ method and be consistent with it.
-
-    Note:
-        Do not include the `self` parameter in the ``Args`` section.
-
-    Args:
-        msg (str): Human readable string describing the exception.
-        code (:obj:`int`, optional): Error code.
+    """
+    SinglyLinkedList Class
 
     Attributes:
-        msg (str): Human readable string describing the exception.
-        code (int): Exception error code.
+        head (Node): Human readable string describing the exception.
     """
 
     def __init__(self):
         self.__head = None
 
     def __str__(self):
+        """str: string"""
         current_node = self.__head
         sll = ""
         while current_node is not None:
@@ -120,6 +116,13 @@ class SinglyLinkedList(object):
         return sll
 
     def sorted_insert(self, value):
+        """
+        sorted insert function
+
+        Args:
+            value (int): integer
+
+        """
         new_node = Node(value)
         current_node = self.__head
         previous_node = None
