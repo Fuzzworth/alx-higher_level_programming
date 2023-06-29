dc28b2feb13df374974545e0d67d1b6de5e055ab
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-06-30 01:05:50 +0200

    Write a class Node that defines a node of a singly linked list by:

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -115,7 +115,7 @@ class SinglyLinkedList(object):
     def sorted_insert(self, value):
         new_node = Node(value)
         current_node = self.__head
-        previous_node = current_node
+        previous_node = self.__head
         if current_node is None:
             self.__head = new_node
         else:
