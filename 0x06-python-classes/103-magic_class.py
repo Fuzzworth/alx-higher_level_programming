b65f784d9eeb77540ca6c9b17046441e009b69af
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-06-30 02:55:48 +0200

    Write the Python class MagicClass that does exactly the same as the following Python bytecode:

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,13 +1,31 @@
 #!/usr/bin/python3
 import math
-
+"""
+Magic class module
+"""
 
 class MagicClass:
+    """
+    Magic class
+
+    Attribute:
+        radius (int): integer
+    """
     def __init__(self, radius):
+        """
+        init
+
+        Args:
+            radius (int): integer
+
+        Raises:
+            TypeError: not an int
+        """
         self.__radius = 0
         if type(radius) is not int:
             raise TypeError('radius must be a number')
         self.__radius = radius
 
     def circumference(self):
+        """int: radius"""
         return (2 * math.pi) * self.__radius
