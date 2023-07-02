1007dc2a5c8544ef9313fc01fba6d43dc81454bf
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-07-02 22:27:24 +0200

    Write a function that divides all elements of a matrix.

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+Division Module
+"""
+
+def matrix_divided(matrix, div):
+    """
+    function that divides all elements of a matrix
+
+    Args:
+        matrix (:obj:list of :obj:int): Matrix
+        div (int:float): float or integer
+
+    Raises:
+        TypeError: must be lists of lists and Each row
+            must have the same size
+
+    Returns:
+        list:list: new list
+    """
+    new_matrix = []
+    if (not isinstance(number, float) and not isinstance(number, int)):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    if not isinstance(matrix, list):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    try:
+        size = len(matrix[0])
+    except Exception:
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    for i in matrix:
+        if not isinstance(i, list):
+            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        if len(i) != size:
+            raise TypeError("Each row of the matrix must have the same size")
+        new_row = []
+        for number in i:
+            result = float("{:.2f}".format(number / div))
+            new_row.append(result)
+        new_matrix.append(new_matrix)
+    return new_matrix
