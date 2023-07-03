eff36af58a1c864157152f1ccdbff36d7a34e3bc
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-07-03 23:40:48 +0200

    The N queens puzzle is the challenge of placing N non-attacking queens on an N×N chessboard. Write a program that solves the N queens problem.

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -37,7 +37,8 @@ def create_list(board, n):
             if board[i][j] == "Q":
                 matrix.append([i, j])
     return matrix
- 
+
+
 def add_sol(board, ans, n):
     temp = []
     for i in range(n):
@@ -46,62 +47,62 @@ def add_sol(board, ans, n):
             string += board[i][j]
         temp.append(string)
     ans.append(temp)
-     
-     
-def  is_safe(row, col, board, n):
+
+
+def is_safe(row, col, board, n):
     x = row
     y = col
-     
-    while(x>=0):
+
+    while x >= 0:
         if board[x][y] == "Q":
             return False
         else:
             x -= 1
-             
+
     x = row
     y = col
-    while(y<n and x>=0):
+    while(y < n and x >= 0):
         if board[x][y] == "Q":
             return False
         else:
             y += 1
             x -= 1
-             
+
     x = row
     y = col
-    while(y>=0 and x>=0):
+    while(y >= 0 and x >= 0):
         if board[x][y] == "Q":
             return False
         else:
             x -= 1
             y -= 1
-    return True   
- 
- 
+    return True
+
+
 def solveNQueens(row, ans, board, n):
-     
+
     if row == n:
         add_sol(board, ans, n)
         return
-     
+
     for col in range(n):
-         
+
         if is_safe(row, col, board, n):
-             
+
             board[row][col] = "Q"
-            solveNQueens(row+1, ans, board, n)
-             
+            solveNQueens(row + 1, ans, board, n)
+
             board[row][col] = "."
- 
+
 if __name__ == "__main__":
-     
+
     board = generate_matrix(N)
-     
+
     ans = []
     solveNQueens(0, ans, board, N)
-     
-    if ans == []:
-        print("Solution does not exist")
+
+    if len(ans) == 0:
+        print()
     else:
         for i in ans:
             print(create_list(i, N))
