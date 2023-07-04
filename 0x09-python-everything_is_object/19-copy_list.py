f89269fb3b9ddb2b1d847be504e9012f315b12d9
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-07-04 19:35:25 +0200

    19. Copy a list object no docs fix

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,19 +1,3 @@
 #!/usr/bin/python3
-"""
-Module to copy an object
-"""
-
-
 def copy_list(l):
-    """
-    function that returns a copy of a list.
-
-    Args:
-        l (list): list to copy
-
-    Returns:
-        list: copy of l
-    """
-
-    new_list = l[:]
-    return new_list
+    return l[:]
