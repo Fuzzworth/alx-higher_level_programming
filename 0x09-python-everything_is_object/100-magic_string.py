6761373f0d7d31062a84e04ccfad7ab595fc0ecb
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-07-04 22:27:29 +0200

    Fix 1

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-a_list = []
-def magic_string():
-    a_list.append("BestSchool") ; return ", ".join(a_list)
+def magic_string(a_list=[0]):
+    a_list.append("BestSchool")
+    return ", ".join(a_list)
