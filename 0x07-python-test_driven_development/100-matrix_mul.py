29eb4733a090e50a7caecefe3ba40c995edb91ba
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-07-08 00:51:46 +0200

    Write a function that multiplies 2 matrices:

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -46,9 +46,9 @@ def matrix_mul(m_a, m_b):
             raise TypeError("each row of m_a must be of the same size")
         result.append([0] * len(m_b[0]))
         for j in range(len(m_b[0])):
-            if len(m_b[j]) != size_b:
-                raise TypeError("each row of m_b must be of the same size")
             for k in range(len(m_b)):
+                if len(m_b[j]) != size_b:
+                    raise TypeError("each row of m_b must be of the same size")
                 if not isinstance(m_a[i][k], (int, float)):
                     raise TypeError("m_a should contain only integers or floats")
                 if not isinstance(m_b[k][j], (int, float)):
