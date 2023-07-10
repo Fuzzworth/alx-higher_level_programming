1cbfeace427a135f25095912d4740c3829fd7f69
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-07-10 15:46:40 +0200

    Write a class MyList that inherits from list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -12,4 +12,5 @@ class MyList(list):
         prints a sorted list
         """
         sorted_list = super().copy()
-        print(sorted_list.sort())
+        sorted_list.sort()
+        print(sorted_list)
