6f85ba06482fcf894a94955c2be5eaedd3c9ff65
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-07-10 22:13:14 +0200

    fix 2

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -9,14 +9,6 @@ class MyList(list):
     Module that inherits from list
     """
 
-    def __init__(self, iterable=[]):
-        """
-        init
-
-        Args:
-            iterable (list): list of things
-        """
-        super().__init__(int(item) for item in iterable)
     def print_sorted(self):
         """
         prints a sorted list
