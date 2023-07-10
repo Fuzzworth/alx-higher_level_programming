1dcbb1ed1b8a17643f76554cfef78d63cdba8776
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-07-11 00:57:18 +0200

    Write a class Square that inherits from Rectangle (9-rectangle.py):

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Module docs
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    class docs
+    """
+    def __init__(self, size):
+        """
+        constructor docs
+        """
+        super().integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """
+        area
+        """
+        return self.__size * self.__size
