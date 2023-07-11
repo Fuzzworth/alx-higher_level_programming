f8bbebfcb3e38f850282dce7e5c1c9f2149f8e33
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-07-11 23:10:59 +0200

    12. Pascal's Triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+module doc
+"""
+
+
+def pascal_triangle(n):
+    """
+    function docs
+    """
+    final_list = []
+    if n <= 0:
+        return final_list
+    for i in range(n):
+        new_row = []
+        num = 1
+        for j in range(i+1):
+            num = num * (i - j) // j
+            new_row.append(num)
+        final_list.append(new_row)
+            
