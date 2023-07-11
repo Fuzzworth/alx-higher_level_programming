754a9b75c64f31a1bedf6692fea2b527c701d9a3
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-07-12 00:47:58 +0200

    Fix 2

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -2,7 +2,7 @@
 """
 Module docs
 """
-import sys, traceback
+import sys
 
 
 i = 0
@@ -16,23 +16,22 @@ try:
             myKeys = list(code_dict.keys())
             myKeys.sort()
             sorted_dict = {p: code_dict[p] for p in myKeys}
-            print(f"File size: {total_size}")
+            print("File size: {:d}".format(total_size))
             for c in sorted_dict:
                 print("{}: {}".format(c, sorted_dict[c]))
         stripped = line.split()
-        code = stripped[status_code_index]
-        file_size = stripped[file_size_index]
+        code = int(stripped[status_code_index])
         if code in code_dict:
             code_dict[code] = code_dict[code] + 1
         else:
             code_dict[code] = 1
-        total_size += int(file_size)
-        i += int(1)
+        file_size = int(stripped[file_size_index])
+        total_size += file_size
+        i += 1
 except KeyboardInterrupt:
     myKeys = list(code_dict.keys())
     myKeys.sort()
     sorted_dict = {p: code_dict[p] for p in myKeys}
-    print(f"File size: {total_size}")
+    print("File size: {:d}".format(total_size))
     for c in sorted_dict:
         print("{}: {}".format(c, sorted_dict[c]))
-    traceback.print_exc(file=sys.stdout)
