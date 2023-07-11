3e5bcdd25309dadf231f1400c9ca180abe612469
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-07-11 23:00:05 +0200

    Write a class Student that defines a student by: (based on 10-student.py)

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+Mod docs
+"""
+
+
+class Student:
+    """
+    class docs
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        function docs
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        function doc
+        """
+        final_list = {}
+        all_string = True
+        if type(attrs) is not list:
+            all_string = False
+
+        if type(attrs) is list:
+            for i in attrs:
+                if type(i) != str:
+                    all_string = False
+        if not all_string:
+            return self.__dict__
+        else:
+            for i in attrs:
+                if i in self.__dict__:
+                    final_list[i] = self.__dict__[i]
+            return final_list
+
+    def reload_from_json(self, json):
+        """
+        function docs
+        """
+        for i in json:
+            self.__dict__[i] = json[i]
