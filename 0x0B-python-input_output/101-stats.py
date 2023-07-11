f7a48c9edd1e89f658f146178044fa2b936a586a
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-07-11 23:52:52 +0200

    Write a script that reads stdin line by line and computes metrics:

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -4,6 +4,27 @@ Module docs
 """
 import sys
 
-
+i = 0
+code_dict = {}
+total_size = 0
 for line in sys.stdin:
-    print(line)
+    if i != 0 and i % 10 == 0:
+        print(f"File size: {total_size}")
+        for i in code_dict:
+            print("{:d}: {:d}".format(i, code_dict[i]))
+        code_dict.clear()
+        total_size = 0
+    stripped = line.strip(" ")
+    if len(stripped) == 6:
+        file_size_index = 5
+        status_code_index = 4
+    else:
+        file_size_index = 4
+        status_code_index = 3
+    code = stripped[status_code_index]
+    if code in code_dict:
+        code_dict[code] = code_dict[code] + 1
+    else:
+        code_dict[code] = 1
+    total_size += stripped[file_size_index]
+    i += 1
