f867393ecb36c9f666edcb20bf4f06a14e7885f2
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-07-12 01:19:54 +0200

    Fix 5

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -25,6 +25,7 @@ def stats():
     total_size = 0
     file_size_index = 8
     status_code_index = 7
+    valid_index = [200, 301, 400, 401, 403, 404, 405, 500]
     i = 0
     try:
         for line in sys.stdin:
@@ -32,14 +33,15 @@ def stats():
                 print_stats(code_dict, total_size)
             stripped = line.split()
             code = int(stripped[status_code_index])
-            if code in code_dict:
-                code_dict[code] = code_dict[code] + 1
-            else:
-                code_dict[code] = 1
+            if code in valid_index:
+                if code in code_dict:
+                    code_dict[code] = code_dict[code] + 1
+                else:
+                    code_dict[code] = 1
             file_size = int(stripped[file_size_index])
             total_size += file_size
             i += 1
-    except KeyboardInterrupt as e:
+    except (Exception, KeyboardInterrupt):
         print_stats(code_dict, total_size)
         raise
 
