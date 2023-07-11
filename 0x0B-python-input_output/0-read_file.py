b932b7cbe49b30253886f8a9284c1152796147f0
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-07-11 21:07:24 +0200

    Fix: 1

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,6 +5,9 @@ Mod docs
 
 
 def read_file(filename=""):
+    """
+    function doc
+    """
     with open(filename, encoding="utf-8") as f:
         for line in f:
             print(line, end = "")
