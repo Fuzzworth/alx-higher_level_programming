48b343266f91d51e961cb9097976893cf090a561
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-07-16 16:46:43 +0200

    Write the class Square that inherits from Rectangle:

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Module Docs
+"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+    Class Docs
+    """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        Function Docs
+        """
+
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """str: Function call"""
+
+        return "[Square] ({:d}) {:d}/{:d} - {:d}/{:d}".format(self.id,
+                                                                 self.x, self.y,
+                                                                 self.width, self.height)
