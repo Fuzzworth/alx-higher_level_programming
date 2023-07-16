d3fdef40be886b17dedce67921876a4357323355
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-07-16 18:06:21 +0200

    Update the class Base by adding the class method def create(cls, **dictionary): that returns an instance with all attributes already set:

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -71,5 +71,6 @@ class Base:
             dummy_obj = cls(dictionary["width"], dictionary["height"])
         elif "size" in dictionary:
             dummy_obj = cls(dictionary["size"])
+        print(dummy_obj.id)
         dummy_obj.update(dictionary)
         return dummy_obj
