c5de8d7fe42cf0361707158f50e2fe85aa0c1172
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-07-17 23:01:49 +0200

    Clean up

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -164,8 +164,8 @@ class Base:
         if isinstance(obj_item, Base):
             list_item.append(obj_item.id)
             if cls.__name__ == "Rectangle":
-                    list_item.append(obj_item.width)
-                    list_item.append(obj_item.height)    
+                list_item.append(obj_item.width)
+                list_item.append(obj_item.height)    
             elif cls.__name__ == "Square":
                 list_item.append(obj_item.size)
             list_item.append(obj_item.x)
