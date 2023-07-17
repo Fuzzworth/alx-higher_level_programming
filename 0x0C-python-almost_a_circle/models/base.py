7fe59321ada46ac4a3e599114a8a847387afe89c
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-07-17 19:28:35 +0200

    Update the class Base by adding the class methods def save_to_file_csv(cls, list_objs): and def load_from_file_csv(cls): that serializes and deserializes in CSV:

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -135,14 +135,14 @@ class Base:
         dictionary_row = {}
         if type(row) is list:
             dictionary_row = []
-            if cls.__name__ = "Rectangle":
+            if cls.__name__ == "Rectangle":
                 if len(row) == 5:
                     dictionary_row["id"] = row[0]
                     dictionary_row["width"] = row[1]
                     dictionary_row["height"] = row[2]
                     dictionary_row["x"] = row[3]
                     dictionary_row["y"] = row[4]
-            elif cls.__name__ = "Square":
+            elif cls.__name__ == "Square":
                 if len(row) == 4:
                     dictionary_row["id"] = row[0]
                     dictionary_row["size"] = row[1]
