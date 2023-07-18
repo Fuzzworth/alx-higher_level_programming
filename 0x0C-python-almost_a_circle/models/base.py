341452d96edfc7cdfa56dc8119a7b4c01250257a
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-07-18 03:06:51 +0200

    fix: 1 ;Task 11

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -189,7 +189,7 @@ class Base:
         t = turtle.Turtle()
         turtle.colormode(255)
         def exit_on_click(i, j):
-            screen.done()
+            t.done()
         screen.onclick(exit_on_click)
         screen.listen()
         r = False
