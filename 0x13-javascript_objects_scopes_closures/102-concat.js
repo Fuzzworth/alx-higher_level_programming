ca8ca2ced900598fe2699297e436e1f090fc4ac5
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-09-13 02:18:17 +0200

    Final Task 13

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+const fs = require('fs');
+const fileOne = fs.readFileSync(process.argv[2], 'utf8');
+const fileTwo = fs.readFileSync(process.argv[3], 'utf8');
+fs.writeFileSync(process.argv[4], fileOne + fileTwo);
