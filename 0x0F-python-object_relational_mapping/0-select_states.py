254be223b62e9fe178710f3f7675c5508295e22d
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-09-18 00:14:12 +0200

    Task 0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Module Docs
+"""
+from sys import argv
+import MySQLdb
+
+
+def main():
+    """
+    Function DOC
+    """
+    db_user = argv[1]
+    db_password = argv[2]
+    db_db = argv[3]
+    db_host = "localhost"
+    db_port = 3306
+
+    try:
+        conn = MySQLdb.connect(host=db_host, port=db_port, user=db_user, passwd=db_password, db=db_db, charset="utf8")
+        cur = conn.cursor()
+        cur.execute("SELECT id, name FROM states ORDER BY id ASC")
+        query_rows = cur.fetchall()
+        for row in query_rows:
+            print(row)
+        cur.close()
+        conn.close()
+    except Exception:
+        pass
+
+
+if __name__ == "__main__":
+    main()
