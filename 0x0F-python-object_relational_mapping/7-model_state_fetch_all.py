4599ae1a5eebe960d0bc6ce22b61211574e939ae
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-09-18 03:04:30 +0200

    Task 7.0.1

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Module Docs
+"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
+
+
+def main():
+    """
+    Function DOC
+    """
+    db_user = argv[1]
+    db_password = argv[2]
+    db_db = argv[3]
+    db_host = "localhost"
+
+    try:
+        engine = create_engine(
+                'mysql+mysqldb://{}:{}@{}/{}'
+                .format(db_user,
+                        db_password,
+                        db_host,
+                        db_db),
+                pool_pre_ping=True)
+        Base.metadata.create_all(engine)
+        session = Session(engine)
+        for state in session.query(State).order_by(State.id).all():
+            print("{}: {}".format(state.id, state.name))
+        session.close()
+    except Exception:
+        pass
+
+
+if __name__ == "__main__":
+    main()
