e48a302c991d4937daf0933a97b39d3302d886b2
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-09-18 04:01:18 +0200

    Task 15.0.2

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -14,5 +14,5 @@ class State(Base):
     Class Docs
     """
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False,unique=True)
+    id = Column(Integer, primary_key=True, nullable=False, unique=True)
     name = Column(String(128), nullable=False)
