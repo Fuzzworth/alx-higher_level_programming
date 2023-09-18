4ce6dd6ad189c55eba5c79efb6bbf546df0a476d
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-09-18 02:19:43 +0200

    Task 5.0.1

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -35,10 +35,11 @@ def main():
         cur = conn.cursor()
         cur.execute(qs, (state_name, ))
         query_rows = cur.fetchall()
-        list_result = []
-        for row in query_rows:
-            list_result.append(row[0])
-        print(", ".join(list_result))
+        if query_rows:
+            list_result = []
+            for row in query_rows:
+                list_result.append(row[0])
+            print(", ".join(list_result))
         cur.close()
         conn.close()
     except Exception:
