4c1a10ac921e7bebbc4bd64343793902a2feaa51
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-09-18 03:32:42 +0200

    Task 10.0.2

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -18,25 +18,22 @@ def main():
     db_host = "localhost"
     search_string = argv[4]
 
-    try:
-        engine = create_engine(
-                'mysql+mysqldb://{}:{}@{}/{}'
-                .format(db_user,
-                        db_password,
-                        db_host,
-                        db_db),
-                pool_pre_ping=True)
-        Base.metadata.create_all(engine)
-        Session = sessionmaker(bind=engine)
-        session = Session(engine)
-        state = session.query(State).filter(State.name == argv[4]).first()
-        if state:
-            print(state.id)
-        else:
-            print("Not found")
-        session.close()
-    except Exception:
-        pass
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@{}/{}'
+            .format(db_user,
+                    db_password,
+                    db_host,
+                    db_db),
+            pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = session.query(State).filter(State.name == search_string).first()
+    if state:
+        print(state.id)
+    else:
+        print("Not found")
+    session.close()
 
 
 if __name__ == "__main__":
