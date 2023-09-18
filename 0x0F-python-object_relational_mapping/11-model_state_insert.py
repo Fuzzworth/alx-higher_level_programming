ca3b3282a4e4f4e1d9c2528798cebcedf5e10da6
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-09-18 03:35:57 +0200

    Task 11.0.2

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+Module Docs
+"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session, sessionmaker
+
+
+def main():
+    """
+    Function DOC
+    """
+    db_user = argv[1]
+    db_password = argv[2]
+    db_db = argv[3]
+    db_host = "localhost"
+
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@{}/{}'
+            .format(db_user,
+                    db_password,
+                    db_host,
+                    db_db),
+            pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = State(name='Louisiana')
+    session.add(state)
+    session.commit()
+    if state:
+        print(state.id)
+    else:
+        print("Not found")
+    session.close()
+
+
+if __name__ == "__main__":
+    main()
