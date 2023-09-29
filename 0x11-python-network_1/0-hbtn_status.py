dd6b177f8695e9aeec65b9449015362eb6da43b3
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-09-29 11:50:06 +0200

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,6 +9,11 @@ url = 'https://alx-intranet.hbtn.io/status'
 with urllib.request.urlopen(url) as response:
     html = response.read()
     header = response.getheader("Content-type")
-    print(html)
-    print(header)
-    print(type(html))
+    print("Body response:")
+    print("\t- type: {}".format(type(html)))
+    print(f"\t- content: {html}")
+    if "utf-8" in header:
+        print("\t- utf8 content: {}".format("OK"))
+    else:
+        print("\t- utf8 content: {}".format("BAD"))
+
