e3e4d51f228e20737b18300ae556a5763acb78a2
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-09-29 12:03:43 +0200

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,7 +8,7 @@ import urllib.request
 url = 'https://alx-intranet.hbtn.io/status'
 with urllib.request.urlopen(url) as response:
     html = response.read()
-    header = response.header_items()
+    header = response.headeritems
     print("Body response:")
     print("\t- type: {}".format(type(html)))
     print(f"\t- content: {html}")
