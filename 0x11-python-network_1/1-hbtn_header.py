1a3db31aa3e729ae83d66fbd68655ba798ce7c89
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-09-29 12:11:04 +0200

    task 1

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module Doc
+"""
+from sys import argv
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    with urllib.request.urlopen(url) as response:
+        html = response.read().getparameter("X-Request-Id")
+        print(f"{html}")
