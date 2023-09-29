0e52628eea49eea936ffb14f08bd370a87b8c441
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-09-29 12:07:20 +0200

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,10 +5,11 @@ Module Doc
 import urllib.request
 
 
-url = 'https://alx-intranet.hbtn.io/status'
-with urllib.request.urlopen(url) as response:
-    html = response.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(html)))
-    print(f"\t- content: {html}")
-    print("\t- utf8 content: {}".format("OK"))
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print(f"\t- content: {html}")
+        print("\t- utf8 content: {}".format(html.decode("utf-8")))
