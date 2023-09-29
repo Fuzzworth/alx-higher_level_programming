6dbe395c789ce3193c5ea4ffdd1064a7588154f1
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-09-29 23:50:01 +0200

    task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Module Docs
+"""
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    c = ""
+    if len(argv) > 1:
+        c = {"q": argv[1][0]} 
+    req = requests.post(url, data=c)
+    try:
+        res = req.json()
+        if res:
+            str_re = "[{}] {}"
+            print(str_re.format(res.get("id"), res.get("name")))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
