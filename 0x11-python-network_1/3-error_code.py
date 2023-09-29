aa43e9fdc1d9df9fbd6f75e26163f8271efb17f5
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-09-29 23:36:22 +0200

    task 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module Docs
+"""
+from sys import argv
+from urllib.error import HTTPError
+from urllib.request import urlopen, Request
+
+
+if __name__ == "__main__":
+    try:
+        with urlopen(Request(argv[1])) as response:
+            html = response.read()
+            print(html.decode('utf-8'))
+    except HTTPError as excption:
+        print('Error code:', exception.code)
