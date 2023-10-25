fab3fd02b3b167f868c7ae218049e8d4678d915b
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-10-25 02:40:45 +0200

    Task 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
-let url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+console.log(url)
 
 request(url, function (error, response, body) {
   if (error) {
