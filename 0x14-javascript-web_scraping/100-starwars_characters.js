a8409ff016a9117c2e6566b3d5425b76fa7d2e20
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-10-25 03:51:35 +0200

    task 7;fix 1

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -6,6 +6,7 @@ request.get(url, (error, response, body) => {
     console.log(error);
   }
   for (let i = 0; i < JSON.parse(body).characters.length; i++)  {
+	  console.log(i);
     console.log(JSON.parse(body).characters[i]);
     request.get(JSON.parse(body).characters[i], (errorInner, responseInner, bodyInner) => {
       if (errorInner) {
