3dd389a14012da795085050f4af723ed4e0be29e
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-10-25 03:02:52 +0200

    task 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,18 +2,16 @@
 const request = require('request');
 
 request(process.argv[2], function (error, response, body) {
-if (error) {
-console.log(error);
-} else if (response.statusCode === 200) {
-const completed = {};
-const tasks = JSON.parse(body);
-for (const task of tasks) {
-if (task.completed) {
-completed[task.userId] = (completed[task.userId] || 0) + 1;
-}
-}
-console.log(completed);
-} else {
-console.log('An erroror occured. Status code: ' + response.statusCode);
-}
+  if (error) {
+    console.log(error);
+  } else if (response.statusCode === 200) {
+    const completed = {};
+    const tasks = JSON.parse(body);
+    for (const task of tasks) {
+      if (task.completed) {
+        completed[task.userId] = (completed[task.userId] || 0) + 1;
+      }
+    }
+    console.log(completed);
+  }
 });
