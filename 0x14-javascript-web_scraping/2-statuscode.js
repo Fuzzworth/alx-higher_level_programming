f78846f24f96a14bc8d2d7f9a8ab0d80c10f499d
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-10-25 03:44:34 +0200

    Task 2;fix 1

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
 
-request(process.argv[2], function (error, response) {
+request(process.argv[2], (error, response, body) => {
   if (error) {
     console.log(error);
   } else {
