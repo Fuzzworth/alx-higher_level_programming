62dad748a171ce2b57e111fa66af2bb6f68871ee
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-10-25 02:55:00 +0200

    Taks 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,9 +2,8 @@
 const request = require('request');
 
 request(process.argv[2], (error, response, body) => {
-if (error)
-{
-console.log(error);
-}
-console.log(body.split('/people/18/').length - 1);
+  if (error) {
+    console.log(error);
+  }
+  console.log(body.split('/people/18/').length - 1);
 });
