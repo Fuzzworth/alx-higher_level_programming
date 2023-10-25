cd78c003f2491aa8b82230e7bc11816ef15ddff4
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-10-25 02:54:49 +0200

    Taks 4

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const request = require('request');
-const url = "https://swapi-api.alx-tools.com/api/films/"+ process.argv[2] 
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 
-request(url , (error, response, body) => {
+request(url, (error, response, body) => {
   if (error) console.log(error);
   console.log(JSON.parse(body).title);
 });
---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const request = require('request');
+
+request(process.argv[2], (error, response, body) => {
+if (error)
+{
+console.log(error);
+}
+console.log(body.split('/people/18/').length - 1);
+});
