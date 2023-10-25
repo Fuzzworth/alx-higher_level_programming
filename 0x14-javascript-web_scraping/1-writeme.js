ff3257ef76b06be8535b54e6c519b87ef940412a
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-10-25 02:32:48 +0200

    Task 1

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const fileName = process.argv[2];
+const str = process.argv[3];
+
+fs.writeFile(fileName, str, 'utf-8', function (error) {
+  if (error){
+	  console.log(error);
+  }
+});
