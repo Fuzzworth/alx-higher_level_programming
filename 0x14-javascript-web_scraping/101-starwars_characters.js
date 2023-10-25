97fad08a0cc4a7b68ba2d144b894694287315353
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-10-25 03:39:40 +0200

    task 3:fix 1

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -3,7 +3,7 @@ const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request(url, { json: true }, (error, response, body) => {
   if (!error) {
-    for (let i = 0; i < body.characters.length; i++) { 
+    for (let i = 0; i < body.characters.length; i++) {
       request(body.characters[i], { json: true }, (errorInner, responseInner, bodyInner) => {
         if (!errorInner) {
           console.log(bodyInner.name);
--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,6 +3,8 @@ const request = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 
 request(url, (error, response, body) => {
-  if (error) console.log(error);
-  console.log(JSON.parse(body).title);
+if (error){
+console.log(error);
+}
+console.log(JSON.parse(body).title);
 });
