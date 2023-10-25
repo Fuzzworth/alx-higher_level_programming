56dba36c5b13cbbc8ea7e1f2fa4b1377c2b10550
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-10-25 03:24:01 +0200

    Task 8

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -4,11 +4,15 @@ const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request(url, { json: true }, (error, response, body) => {
   if (!error) {
     for (const charUrl of body.characters) {
-      request(charUrl, { json: true }, (error, response, body) => {
-        if (!error) {
-          console.log(body.name);
-        }
+      request(charUrl, { json: true }, (errorInner, responseInner, bodyInner) => {
+        if (!errorInner) {
+          console.log(bodyInner.name);
+	} else {
+	  console.log(errorInner);
+	}
       });
     }
+  } else {
+    console.log(error);
   }
 });
