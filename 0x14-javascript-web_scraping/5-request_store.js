8ad3d4a38602f8d37c626eb7e927bc6eaecd42fe
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-10-25 02:57:17 +0200

    Task 5

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const fileName = process.argv[3];
+
+request(url, (error, response, body) => {
+if (error){
+
+console.log(err);
+}
+try {
+fs.writeFileSync(fileName, body, { encoding: 'utf8' });
+} catch (errot) {
+console.error(errot);
+}
+});
