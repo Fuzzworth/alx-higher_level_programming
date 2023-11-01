7ce324c2552cafbb1d01dc1f179f8396e6014a04
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-11-01 22:05:25 +0200

    task 4

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+  // Add a click event handler to the div with the id 'toggle_header'
+  $("#toggle_header").click(function () {
+    // Select the 'header' element and toggle the "red" and "green" classes
+    $("header").toggleClass("red green");
+  });
+});
