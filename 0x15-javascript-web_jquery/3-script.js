dbc30eb0910f2ecedc67d0efa36c2ef28ca3abd3
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-11-01 22:03:00 +0200

    task 3

---
+++ 0x15-javascript-web_jquery/3-script.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+  // Add a click event handler to the div with the id 'red_header'
+  $("#red_header").click(function () {
+    // Select the 'header' element and add the 'red' class to it
+    $("header").addClass("red");
+  });
+});
