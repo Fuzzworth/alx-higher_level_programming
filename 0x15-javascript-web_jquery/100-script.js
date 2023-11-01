27f98dc0fc867927f3e462ee0aacb277c4aa1e78
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-11-01 22:12:51 +0200

    task 10

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Select the <header> element
+  var header = document.querySelector("header");
+
+  // Update the text color of the <header> element to red
+  if (header) {
+    header.style.color = "#FF0000";
+  }
+});
